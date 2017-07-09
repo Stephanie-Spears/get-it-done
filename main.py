4f1fbc2827cb193194d0fd89689d89cdd230810d
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-07-08 23:42:27 -0700

    add User class, create login.html and register.html templates

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-#installed Flask_Migrate onto flask-env, can be accessed with:
-#from flask_migrate import Migrate
-#migrate = Migrate(app, db)
-#Now I can create a migration repository with "flask db init" in terminal. Adds a migation folder to terminal. After that, can generate initial migration with "flask db migrate". Finally, you can apply the migration to the database with "flask db upgrade". Then each time the database models change, repeat the migrate and upgrade commands. To sync the db in another system just refresh migrations folder from source control and run the upgrade command. to see all commands, "flask db --help"
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-#8888 on mine, but sql says 8889
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:1alpha1@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
@@ -23,9 +18,28 @@ class Task(db.Model):
         self.completed = False
 
 
+class User(db.Model):
+    id = id.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
     if request.method == 'POST':
         task_name = request.form['task']
         new_task = Task(task_name)
@@ -39,7 +53,6 @@ def index():
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
-
     task_id = int(request.form['task-id'])
     task = Task.query.get(task_id)
     task.completed = True
