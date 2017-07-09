f5c080924c3802157b418be65a1e3b6f1c1b06fb
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-07-08 23:09:08 -0700

    update through walkthrough 4, with annotation

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ db = SQLAlchemy(app)
 
 #class extends db object (with class "model" inside it) which we specified above. Model allows us to do stuff like queryAll
 class Task(db.Model):
+#any time we modify the Model class with a new column, we will have to use drop_all() and then create_all() in order for those changes to be viewed in the database itself (losing anything in the db). Theres also Flask-Migrate, which doesn't make you lose all your data (but we haven't learned it yet)
 #data associated with id field of Task class will go into my db column, as an integer, and representing the primary key for this class and database associated with this tasks
     id = db.Column(db.Integer, primary_key=True)
     #column in database called name, using string of 120 max char
     name = db.Column(db.String(120))
 
-#new property for our task class, of Boolean type--specifying a default value
-    completed = db.Column(db.Boolean, default=False)
+#new property for our task class, of Boolean type--you can specify a default value with ,default=False
+    completed = db.Column(db.Boolean)
 
     def __init__(self, name):
         self.name = name
@@ -34,24 +35,40 @@ def index():
         db.session.add(new_task)
         db.session.commit()
 
-#pulls all Task objects from the database
-    tasks = Task.query.all()
-    return render_template('todos.html',title="Get It Done!", tasks=tasks)
+#pulls all Task objects from the database(even the "deleted" ones which have been set to False. We can fix it so deleted wont show by adding a filter to the query)
+    # tasks = Task.query.all()
+    tasks = Task.query.filter_by(completed=False).all()
+#give completed tasks it's own section to be displayed on, and remember to pass it into the render_template just like we did with tasks=tasks--and then actually DO something with it in the template by updating the html
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('todos.html',title="Get It Done!", tasks=tasks, completed_tasks=completed_tasks)
 
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
 
-#we have to convert the task_id parameter into an int because it tries to send it as a string
     task_id = int(request.form['task-id'])
-    #passing task_id to get so we can get the specific task from the database
     task = Task.query.get(task_id)
-    #asking the database to delete that specific task, and then commit it (which is what actually runs the queries to the database to be completed)
-    db.session.delete(task)
+    task.completed = True
+    db.session.add(task)
     db.session.commit()
 
-#not returning content from this request, so we are redirecting them to the main page after the query finishes
     return redirect('/')
+
+
+#old delete-task handler below:
+# @app.route('/delete-task', methods=['POST'])
+# def delete_task():
+#
+# #we have to convert the task_id parameter into an int because it tries to send it as a string
+#     task_id = int(request.form['task-id'])
+#     #passing task_id to get so we can get the specific task from the database
+#     task = Task.query.get(task_id)
+#     #asking the database to delete that specific task, and then commit it (which is what actually runs the queries to the database to be completed)
+#     db.session.delete(task)
+#     db.session.commit()
+#
+# #not returning content from this request, so we are redirecting them to the main page after the query finishes
+#     return redirect('/')
 
 
 if __name__ == '__main__':
