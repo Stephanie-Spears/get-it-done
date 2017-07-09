5217e904312167ab66509336662e70ca6c462fc2
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-07-08 23:52:31 -0700

    minor typos in main

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Task(db.Model):
 
 
 class User(db.Model):
-    id = id.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
 
